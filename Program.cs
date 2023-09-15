f08ec134999bd077b1ce9f5765b32a45b8a86927
Rayan Perera <rayanp2004@gmail.com>
2023-09-15 12:50:21 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿//B D I R Perera
+//COCSE234F-031
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity_8
+{
+    class product
+    {
+        protected int rawmaterialcost;
+        protected int productioncost;
+
+        public  void setdetails()
+        {
+            Console.WriteLine("Enter Raw Material Cost :");
+            rawmaterialcost=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Prodcution Cost");
+            productioncost =Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public virtual void findtotalcost()
+        {
+
+            int totalcost = rawmaterialcost + productioncost;
+            Console.WriteLine("your total cost : " + totalcost);
+
+
+        }
+
+    }
+
+    class OrderedProduct:product
+    {
+        private int deliverycost;
+
+        public  void setdeliverycost()
+        {
+            Console.WriteLine("enter the delivery cost");
+            deliverycost = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public override void findtotalcost()
+        {
+            
+            int totalcost = rawmaterialcost + productioncost + deliverycost;
+            Console.WriteLine("Total Cost (including delivery cost) : " + totalcost);
+
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------WELCOME-------");
+            Console.WriteLine();
+            product product = new product();
+            OrderedProduct orderedProduct1 = new OrderedProduct();
+            orderedProduct1.setdetails();
+            orderedProduct1.setdeliverycost();
+            Console.WriteLine("---------------------------------------");
+            orderedProduct1.findtotalcost();
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
